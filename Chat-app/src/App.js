58fb1302adf55b33ba2061b353b4738fb642c4d0
Tanay-Shah <tanay.shah2558@gmail.com>
2024-05-25 19:39:22 +0530

    Neww

--- Chat-app/src/App.js
+++ Chat-app/src/App.js
@@ -1,13 +1,21 @@
 import Router from "./Routes";
-
-
+import { Paper, ThemeProvider,createTheme } from "@mui/material";
+const theme=createTheme({
+  palette:{
+    mode:"dark"
+  }
+})
 
 function App() {
 
 
   return (
     <>
+    <ThemeProvider theme={theme}>
+    <Paper>
     <Router/>
+    </Paper>
+    </ThemeProvider>
     </>
   );
 }
