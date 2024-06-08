390040cd7bbd5ba79dc14f3340d6201a63b5e843
Tanay-Shah <tanay.shah2558@gmail.com>
2024-06-08 14:30:09 +0530

    "Adding Contact-Info Section"

--- Chat-app/src/Pages/Dashboard/GeneralApp.js
+++ Chat-app/src/Pages/Dashboard/GeneralApp.js
@@ -2,19 +2,23 @@ import React from "react";
 import Chat from "./chat";
 import { Box, Stack } from "@mui/material";
 import Conversation from "../../components/conversation";
+import Contact from "../../components/contact";
 
 export default function GeneralApp() {
   return (
     <Stack direction="row" sx={{ width: "100%" }}>
       {/* Chats */}
       <Stack maxWidth={320}>
-      <Chat />
+        <Chat />
       </Stack>
 
       {/* Conversation */}
-      <Box sx={{ height: "100%", width: "calc(100vw - 420px)" }}>
+      <Box sx={{ height: "100%", width: "calc(100vw - 750px)" }}>
         <Conversation />
       </Box>
+
+      {/* Contact info */}
+      <Contact />
     </Stack>
   );
 }
