0c92112f5ffe7eccc044cd9fe3889a2089e5d3bb
Tanay-Shah <tanay.shah2558@gmail.com>
2024-06-10 09:44:48 +0530

    git Safe

--- Chat-app/src/components/contact.js
+++ Chat-app/src/components/contact.js
@@ -2,21 +2,23 @@ import React from "react";
 import { Box, Button } from "@mui/material";
 import { useSelector ,useDispatch} from "react-redux";
 import {toggleSidebar} from '../redux/features/slices'
-
+import {username} from '../var.js'
 
 const Contact = () => {
-  const open = useSelector(state=>state.slice)
+  // const open = useSelector(state=>state.slice)
   const dispatch=useDispatch();
+
+  console.log(username);
+
 
   function handleToggle(action){
     dispatch(toggleSidebar(action))
   }
 
-  console.log(open);
-
   return (
     <Box sx={{ height: "100%", width: 320, backgroundColor: "red" }}>
     <Button onClick={()=>handleToggle('IMAGE')}>CLICK ME</Button>
+    {username}
     </Box>
   );
 };
