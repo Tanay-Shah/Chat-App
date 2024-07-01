760936ed0c117a9cec1fd5ee7ee6849e1f6776b4
Tanay-Shah <tanay.shah2558@gmail.com>
2024-07-01 13:48:58 +0530

    ""

--- Chat-app/src/Pages/Dashboard/Settings.js
+++ Chat-app/src/Pages/Dashboard/Settings.js
@@ -1,5 +1,12 @@
-import { useTheme, Stack, Typography, IconButton, Avatar } from "@mui/material";
-import { CaretLeft } from "phosphor-react";
+import {
+  useTheme,
+  Stack,
+  Typography,
+  IconButton,
+  Avatar,
+  Divider,
+} from "@mui/material";
+import { Bell, CaretLeft } from "phosphor-react";
 import { faker } from "@faker-js/faker";
 
 import React from "react";
@@ -29,7 +36,7 @@ const Settings = () => {
         }}
       >
         {/* Header */}
-        <Stack direction={"row"} alignItems={"center"} spacing={3} pt={2}>
+        <Stack direction={"row"} alignItems={"center"} pt={2}>
           <IconButton>
             <CaretLeft size={30} />
           </IconButton>
@@ -39,9 +46,72 @@ const Settings = () => {
         </Stack>
 
         {/* User Info */}
-        <Stack direction={"row"} alignItems={"center"} spacing={3} pt={2}>
-          <Avatar src={faker.image.avatar()} />
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          spacing={3}
+          pt={2}
+          pl={2}
+        >
+          <Avatar sx={{ width: 56, height: 56 }} src={faker.image.avatar()} />
+          <Stack>
+            <Typography variant={"h6"} fontWeight={550}>
+              Tanay Shah
+            </Typography>
+            <Typography variant={"body2"}>
+              Who decides what your limit is!!
+            </Typography>
+          </Stack>
         </Stack>
+
+        <Stack direction={"row"} spacing={4} pt={8}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Notifications</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
+        <Stack direction={"row"} spacing={4}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Privacy</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
+        <Stack direction={"row"} spacing={4}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Security</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
+        <Stack direction={"row"} spacing={4}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Theme</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
+        <Stack direction={"row"} spacing={4}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Chat Wallpaper</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
+        <Stack direction={"row"} spacing={4}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Request Account info</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
+        <Stack direction={"row"} spacing={4}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Keyboard shortcuts</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
+        <Stack direction={"row"} spacing={4}>
+          <Bell size={20} />
+          <Typography variant={"body1"}>Help</Typography>
+        </Stack>
+        <Divider />
+        {/* //////////////////////// */}
       </Stack>
 
       {/* RIGHT */}
