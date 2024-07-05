cb8dfc1513e881b67f2832f43ed02668b80e4144
Tanay-Shah <tanay.shah2558@gmail.com>
2024-07-05 13:51:25 +0530

    ""

--- Chat-app/src/layouts/main/MainLayout.js
+++ Chat-app/src/layouts/main/MainLayout.js
@@ -1,13 +1,23 @@
+import { Container, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import image from "../../assets/Images/logo.ico";
 
 const MainLayout = () => {
   return (
-    <>
-      <div>MainLayout</div>
+    <Container sx={{ pt: 5, height: "100vh" }} maxWidth="sm">
+      <Stack spacing={5}>
+        <Stack
+          sx={{ width: "100%" }}
+          direction={"column"}
+          alignItems={"center"}
+        >
+          <img style={{ height: 120, width: 120 }} src={image} alt="logo" />
+        </Stack>
+      </Stack>
 
       <Outlet />
-    </>
+    </Container>
   );
 };
 
