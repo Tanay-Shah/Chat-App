cbd2c203f722b5aa1c2fa11dcc7640683fd6f8a7
Tanay-Shah <tanay.shah2558@gmail.com>
2024-07-06 18:14:16 +0530

    ""

--- Chat-app/src/Pages/auth/Login.js
+++ Chat-app/src/Pages/auth/Login.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Link, Typography ,Stack} from "@mui/material";
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import AuthSocial from "../../sections/auth/AuthSocial";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Stack mt={4} spacing={2}>
+      <Typography variant="h4" fontWeight={550}>Login to Tawk</Typography>
+      <Stack direction={"row"} spacing={1} alignItems={"center"}>
+        <Typography variant="body2" fontWeight={550}>New User?</Typography>
+        <Link to="/auth/register" component={RouterLink} fontWeight={550} variant="subtitle2">
+          {" "}
+          Create an account
+        </Link>
+      </Stack>
+      {/* Login Form */}
+      {/* Auth Social */}
+      <AuthSocial/>
+    </Stack>
+  );
+};
 
-export default Login
+export default Login;
--- Chat-app/src/layouts/main/MainLayout.js
+++ Chat-app/src/layouts/main/MainLayout.js
@@ -16,6 +16,9 @@ const MainLayout = () => {
         </Stack>
       </Stack>
 
+
+
+    
       <Outlet />
     </Container>
   );
