d2a4fd585cf3415e5131a3b1df1291ea8ab6ddc2
Tanay-Shah <tanay.shah2558@gmail.com>
2024-07-06 18:27:43 +0530

    ""

--- Chat-app/src/Pages/auth/Login.js
+++ Chat-app/src/Pages/auth/Login.js
@@ -1,4 +1,4 @@
-import { Link, Typography ,Stack} from "@mui/material";
+import { Link, Typography, Stack } from "@mui/material";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import AuthSocial from "../../sections/auth/AuthSocial";
@@ -6,17 +6,27 @@ import AuthSocial from "../../sections/auth/AuthSocial";
 const Login = () => {
   return (
     <Stack mt={4} spacing={2}>
-      <Typography variant="h4" fontWeight={550}>Login to Tawk</Typography>
+      <Typography variant="h4" fontWeight={550}>
+        Login to Tawk
+      </Typography>
       <Stack direction={"row"} spacing={1} alignItems={"center"}>
-        <Typography variant="body2" fontWeight={550}>New User?</Typography>
-        <Link to="/auth/register" component={RouterLink} fontWeight={550} variant="subtitle2">
+        <Typography variant="body2" fontWeight={550}>
+          New User?
+        </Typography>
+        <Link
+          to="/auth/register"
+          underline='hover'
+          component={RouterLink}
+          fontWeight={550}
+          variant="subtitle1"
+        >
           {" "}
           Create an account
         </Link>
       </Stack>
       {/* Login Form */}
       {/* Auth Social */}
-      <AuthSocial/>
+      <AuthSocial />
     </Stack>
   );
 };
--- Chat-app/src/sections/auth/AuthSocial.js
+++ Chat-app/src/sections/auth/AuthSocial.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Divider } from "@mui/material";
+import React from "react";
 
 const AuthSocial = () => {
   return (
-    <div>AuthSocial</div>
-  )
-}
+    <div>
+      <Divider
+        sx={{
+          fontWeight: 550,
+          my: 2.5,
+          color: "text.disabled",
+          "&::before,::after": { borderTopStyle: "dashed" },
+        }}
+      >
+        OR
+      </Divider>
+    </div>
+  );
+};
 
-export default AuthSocial
+export default AuthSocial;
