093ef7cf2b17d8a199bd441b7c40fa749ca20509
Tanay-Shah <tanay.shah2558@gmail.com>
2024-07-09 19:49:06 +0530

    ""

--- Chat-app/src/Pages/auth/Login.js
+++ Chat-app/src/Pages/auth/Login.js
@@ -28,7 +28,7 @@ const Login = () => {
       </Stack>
 
       {/* Login Form */}
-      <LoginForm />
+      <LoginForm type={'Login'}  />
 
       {/* Auth Social */}
       <AuthSocial />
--- Chat-app/src/sections/auth/loginForm.js
+++ Chat-app/src/sections/auth/loginForm.js
@@ -1,9 +1,12 @@
 import { useForm } from "react-hook-form";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Stack, TextField } from "@mui/material";
+import { Button, IconButton, Stack, TextField } from "@mui/material";
+import { Eye, EyeSlash } from "phosphor-react";
+import InputAdornment from "@mui/material/InputAdornment";
 
-function LoginForm() {
+function LoginForm({ type }) {
+  const [eye, seteye] = useState(true);
   const navigate = useNavigate();
 
   // React Form Hook
@@ -24,29 +27,40 @@ function LoginForm() {
     <form onSubmit={handleSubmit(form_submit)}>
       <Stack spacing={3}>
         <TextField
+          type="email"
           id="outlined-basic"
-          label="Name"
+          label="email"
           variant="outlined"
           {...register("Name", { required: true })}
-          aria-invalid={errors.Name ? "true" : "false"}
+          aria-invalid={errors.email ? "true" : "false"}
         />
-        {errors.Name?.type === "required" && (
+        {errors.email?.type === "required" && (
           <p role="alert">First name is required</p>
         )}
 
         <TextField
+          type={eye ? "password" : "text"}
           id="outlined-basic"
           label="Password"
           variant="outlined"
           {...register("Password", { required: true })}
           aria-invalid={errors.Password ? "true" : "false"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => seteye((prev) => !prev)}>
+                  {eye ? <Eye size={28} /> : <EyeSlash size={28} />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
         {errors.Password?.type === "required" && (
           <p role="alert">Password is required</p>
         )}
 
         <Button type={"submit"} variant="contained" sx={{ pt: 1 }}>
-          Login
+          {type}
         </Button>
       </Stack>
     </form>
